263eb71980b28562231e50b31d89dd1ece9eb327
kiennt <trungkien2288@gmail.com>
2013-01-05 12:06:30 +0700

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+gem 'rails', '3.2.10'
+gem 'sqlite3'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+gem "haml", ">= 3.1.6"
+gem "haml-rails", ">= 0.3.4", :group => :development
+gem "bootstrap-sass", ">= 2.0.4.0"
+gem "therubyracer", :group => :assets, :platform => :ruby
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Launchvn::Application.config.secret_token = '42e83f96512b9bd05770c55da4be927bfb6f5e6c8e165182f59fd023d05940c0ed03e6aa39f653a8bde6a32bd57c348ea8444007277a69a4826f7de0b9612761'
