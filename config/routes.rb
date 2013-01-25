fb9abb1a1bfb647950308c33969cb0c3d1b25281
kiennt <trungkien2288@gmail.com>
2013-01-25 12:15:41 +0700

    Remove admin_constraint.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-require 'admin_constraint'
 require 'sidekiq/web'
 
 Launchvn::Application.routes.draw do
--- lib/admin_constraint.rb
+++
@@ -1,9 +0,0 @@
-class AdminConstraint
-  def match?(request)
-    return false
-
-    #return false unless request.session[:user_id]
-    #user = User.find request.session[:user_id]
-    #user.name == 'root'
-  end
-end
