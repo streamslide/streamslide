e0e04a570501c9639cac8a661eca7ca32ba96945
kiennt <trungkien2288@gmail.com>
2013-02-06 12:25:36 +0700

    Remove authentication controller and model

--- app/models/authentication.rb
+++
@@ -1,9 +0,0 @@
-class Authentication < ActiveRecord::Base
-  attr_accessible :provider, :token, :uid, :user_id
-  belongs_to :user
-
-  def update_token auth
-    self.token = auth['credentials']['token']
-    self.save
-  end
-end
