f2f4e3bcf555d4221962f545229965256b8ec98f
dxhuy1988 <doxuanhuy@gmail.com>
2013-03-13 03:02:45 +0900

    add backbone support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,10 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
---
+++ app/assets/javascripts/backbone/launchvn.js.coffee
@@ -0,0 +1,13 @@
+#= require_self
+#= require_tree ./templates
+#= require_tree ./models
+#= require_tree ./views
+#= require_tree ./routers
+
+###
+window.Launchvn =
+  Models: {}
+  Collections: {}
+  Routers: {}
+  Views: {}
+###
