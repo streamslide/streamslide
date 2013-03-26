1825a9e5f27ddd10e48f54e308ce2a8ea27c56b2
dxhuy1988 <doxuanhuy@gmail.com>
2013-03-26 23:05:11 +0900

    add security token

--- app/controllers/fayemessages_controller.rb
+++ app/controllers/fayemessages_controller.rb
@@ -1,14 +1,14 @@
 class FayemessagesController < ApplicationController
   def publish
-    params['data']['content']['ext']['avatar'] = current_user.image_url
+    unless params['data']['content']['ext'].nil?
+      params['data']['content']['ext']['avatar'] = current_user.image_url
+    end
 
     data = params['data']
     channel = params['channel']
-    mes = {:channel => channel, :data => data, :ext => 'token'}
-    
+    mes = {:channel => channel, :data => data, :ext => {:auth_token => ENV['FAYE_TOKEN']}}
     uri = URI.parse(Settings.defaults[:event_server])
     Net::HTTP.post_form(uri, :message => mes.to_json) 
-
     render :nothing => true
   end
 end
